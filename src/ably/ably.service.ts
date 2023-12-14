6371aeb0d2ae6bf094f729360fea45bfdc8df94b
AKK-soft-dev <aungkokosoftdev@gmail.com>
2023-12-14 12:28:48 +0630

    refactor ably service

--- src/ably/ably.service.ts
+++ src/ably/ably.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { Message } from '@prisma/client';
 @Injectable()
 export class AblyService {
+  private Ably: any;
+  private ably: any;
+  constructor() {
+    this.Ably = require('ably');
+    this.ably = new this.Ably.Realtime.Promise(process.env.ABLY_API_KEY);
+  }
   emitMessage(filter: string, message: Message) {
-    const Ably = require('ably');
-    const ably = new Ably.Realtime.Promise(process.env.ABLY_API_KEY);
-    const channel = ably.channels.get('ably-notification');
+    // const Ably = require('ably');
+    // const ably = new Ably.Realtime.Promise(process.env.ABLY_API_KEY);
+    const channel = this.ably.channels.get('ably-notification');
     channel.publish(filter, message);
   }
 }
