777360c4f239e67481791237102d181b72a35980
AKK-soft-dev <aungkokosoftdev@gmail.com>
2023-12-14 18:49:47 +0630

    refactor ably

--- src/ably/ably.service.ts
+++ src/ably/ably.service.ts
@@ -4,14 +4,16 @@ import { Message } from '@prisma/client';
 export class AblyService {
   private Ably: any;
   private ably: any;
+  private channel: any;
+
   constructor() {
     this.Ably = require('ably');
     this.ably = new this.Ably.Realtime.Promise(process.env.ABLY_API_KEY);
+    this.channel = this.ably.channels.get('ably-notification');
   }
   emitMessage(filter: string, message: Message) {
     // const Ably = require('ably');
     // const ably = new Ably.Realtime.Promise(process.env.ABLY_API_KEY);
-    const channel = this.ably.channels.get('ably-notification');
-    channel.publish(filter, message);
+    this.channel.publish(filter, message);
   }
 }
